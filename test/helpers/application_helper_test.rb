27e365d2df848901cd2494391c3dea3d4b972b05
Anne Hoogerbrugge <anne.hoogerbrugge@ixly.nl>
2017-08-15 12:09:26 +0000

    validation start

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-    test "should get new" do
-        assert_equal full_title, " | Ruby on Rails Tutotial Sample App"
-       assert_equal full_title("Sign up"), "Sign up | Ruby on Rails Tutotial"
-    end
+    # test "should get new" do
+    #    assert_equal full_title, " | Ruby on Rails Tutotial Sample App"
+    #   assert_equal full_title("Sign up"), "Sign up | Ruby on Rails Tutotial"
+    #end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   def setup
+     @user = User.new(name: "Example User", email: "user@example.nl")
+   end
+   
+   test "validation user" do
+     assert @user.valid?
+   end
+   
+   test "name should not be empty" do
+     @user.name = "     "
+     assert_not @user.valid?
+   end
+   
+   test "email should not be empty" do
+     @user.email = "     "
+     assert_not @user.valid?
+   end
 end
